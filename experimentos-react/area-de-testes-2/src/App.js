95cb51cbc144dbd2320b01d1e23c075864a5d1e4
Lucas Chagas Henrichs <coding.henrichs@gmail.com>
2021-09-01 22:46:13 -0300

    Deletando os arquivos da area de teste 2

--- experimentos-react/area-de-testes-2/src/App.js
+++ experimentos-react/area-de-testes-2/src/App.js
@@ -2,20 +2,26 @@ import './App.css';
 
 
 import Card from './layout/card/Card.js'
-import NewCalculator from './components/calculator_2.0/NewCalculator';
+//import NewCalculator from './components/calculator_2.0/NewCalculator';
+import LerObjeto from './components/lerObjeto/lerObjeto.js'
 
+const QUADRO_DE_MEDALHAS = [
+  { pais: "Brazil", medalhas: "5" },
+  { pais: "Canada", medalhas: "4" },
+  { pais: "Japão", medalhas: "28" },
+  { pais: "Franca", medalhas: "3" },
+]
 
 function App() {
   return (
     <div className="App">
 
       <Card cardTitle='Component #1'>
-        <NewCalculator/>
+         {QUADRO_DE_MEDALHAS.map(item => <LerObjeto texto={item.pais} numero={item.medalhas} />)}
       </Card> 
 
-      <Card cardTitle='Component #2'>
-      </Card>
       
+
     </div>
   );
 }
